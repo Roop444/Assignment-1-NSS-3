14489834ae752bc241e20016bc8f418c7ad0e895
Roop444 <rupinderk@iiitd.ac.in>
2026-04-20 22:08:44 +0530

    Update sfc-server.c

--- sfc-server.c
+++ sfc-server.c
@@ -1,221 +1,136 @@
+/* =========================
+   sfc-server.c
+   ========================= */
 #include "common.h"
 
+int main()
+{
+    int sfd = socket(AF_INET, SOCK_STREAM, 0);
 
+    struct sockaddr_in addr;
 
-int main(){
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    addr.sin_addr.s_addr = INADDR_ANY;
 
+    bind(sfd, (struct sockaddr *)&addr, sizeof(addr));
+    listen(sfd, 5);
 
+    printf("Server listening on %d\n", PORT);
 
-int sfd=socket(AF_INET,SOCK_STREAM,0);
+    int cfd = accept(sfd, NULL, NULL);
 
+    /* ================= GSS AUTH ================= */
 
+    OM_uint32 maj, min;
 
-struct sockaddr_in addr;
+    gss_ctx_id_t gctx = GSS_C_NO_CONTEXT;
 
-addr.sin_family=AF_INET;
+    gss_buffer_desc in = GSS_C_EMPTY_BUFFER;
+    gss_buffer_desc out = GSS_C_EMPTY_BUFFER;
 
-addr.sin_port=htons(PORT);
+    do {
 
-addr.sin_addr.s_addr=INADDR_ANY;
+        recv_token(cfd, &in);
 
+        maj = gss_accept_sec_context(
+            &min,
+            &gctx,
+            GSS_C_NO_CREDENTIAL,
+            &in,
+            GSS_C_NO_CHANNEL_BINDINGS,
+            NULL,
+            NULL,
+            &out,
+            NULL,
+            NULL,
+            NULL
+        );
 
+        if (out.length > 0) {
+            send_token(cfd, &out);
+            gss_release_buffer(&min, &out);
+        }
 
-bind(sfd,(struct sockaddr*)&addr,sizeof(addr));
+        free(in.value);
 
-listen(sfd,5);
+    } while (maj == GSS_S_CONTINUE_NEEDED);
 
+    if (maj != GSS_S_COMPLETE)
+        gss_die("gss_accept_sec_context", maj, min);
 
+    printf("Authenticated context established.\n");
 
-printf("Server listening on %d\n",PORT);
+    /* ================= KEY ================= */
 
+    unsigned char key[32];
 
+    derive_key(gctx, key);
 
-int cfd=accept(sfd,NULL,NULL);
+    printf("Derived AES key: ");
+    for (int i = 0; i < 8; i++)
+        printf("%02x", key[i]);
+    printf("...\n");
 
+    /* ================= RECEIVE FILE ================= */
 
+    uint32_t n;
+    recv_all(cfd, &n, 4);
 
-/* GSS AUTH */
+    int clen = ntohl(n);
 
+    unsigned char nonce[NONCE_LEN];
+    unsigned char *cipher = malloc(clen);
+    unsigned char tag[TAG_LEN];
 
+    recv_all(cfd, nonce, NONCE_LEN);
+    recv_all(cfd, cipher, clen);
+    recv_all(cfd, tag, TAG_LEN);
 
-OM_uint32 maj,min;
+    printf("Server nonce: ");
+    for (int i = 0; i < NONCE_LEN; i++)
+        printf("%02x", nonce[i]);
+    printf("\n");
 
-gss_ctx_id_t ctx=GSS_C_NO_CONTEXT;
+    /* ================= DECRYPT ================= */
 
+    unsigned char *plain = malloc(clen + 16);
 
+    EVP_CIPHER_CTX *dctx = EVP_CIPHER_CTX_new();
 
-gss_buffer_desc in=GSS_C_EMPTY_BUFFER;
+    int len = 0;
+    int plen = 0;
+    int ret = 0;
 
-gss_buffer_desc out=GSS_C_EMPTY_BUFFER;
+    EVP_DecryptInit_ex(dctx, EVP_aes_256_gcm(), NULL, NULL, NULL);
+    EVP_CIPHER_CTX_ctrl(dctx, EVP_CTRL_GCM_SET_IVLEN, NONCE_LEN, NULL);
+    EVP_DecryptInit_ex(dctx, NULL, NULL, key, nonce);
 
+    EVP_DecryptUpdate(dctx, plain, &len, cipher, clen);
+    plen = len;
 
+    EVP_CIPHER_CTX_ctrl(dctx, EVP_CTRL_GCM_SET_TAG, TAG_LEN, tag);
 
-do{
+    ret = EVP_DecryptFinal_ex(dctx, plain + plen, &len);
 
-    recv_token(cfd,&in);
+    EVP_CIPHER_CTX_free(dctx);
 
-
-
-    maj=gss_accept_sec_context(
-
-        &min,
-
-        &ctx,
-
-        GSS_C_NO_CREDENTIAL,
-
-        &in,
-
-        GSS_C_NO_CHANNEL_BINDINGS,
-
-        NULL,NULL,
-
-        &out,
-
-        NULL,NULL,NULL
-
-    );
-
-
-
-    if(out.length>0){
-
-        send_token(cfd,&out);
-
-        gss_release_buffer(&min,&out);
-
+    if (ret <= 0) {
+        printf("TAG VERIFY FAILED\n");
+        close(cfd);
+        close(sfd);
+        return 1;
     }
 
+    plen += len;
 
+    FILE *fp = fopen("received.out", "wb");
+    fwrite(plain, 1, plen, fp);
+    fclose(fp);
 
-    free(in.value);
-
-
-
-}while(maj==GSS_S_CONTINUE_NEEDED);
-
-
-
-if(maj!=GSS_S_COMPLETE) die("gss_accept_sec_context");
-
-
-
-printf("Authenticated context established.\n");
-
-
-
-/* derive key */
-
-
-
-unsigned char key[32];
-
-derive_key(ctx,key);
-
-
-
-/* receive file */
-
-
-
-uint32_t n;
-
-recv_all(cfd,&n,4);
-
-int clen=ntohl(n);
-
-
-
-unsigned char nonce[NONCE_LEN];
-
-unsigned char *cipher=malloc(clen);
-
-unsigned char tag[TAG_LEN];
-
-
-
-recv_all(cfd,nonce,NONCE_LEN);
-
-recv_all(cfd,cipher,clen);
-
-recv_all(cfd,tag,TAG_LEN);
-
-
-
-/* decrypt */
-
-
-
-unsigned char *plain=malloc(clen+16);
-
-
-
-EVP_CIPHER_CTX *dctx=EVP_CIPHER_CTX_new();
-
-
-
-int len=0,plen=0,ret=0;
-
-
-
-EVP_DecryptInit_ex(dctx,EVP_aes_256_gcm(),NULL,NULL,NULL);
-
-EVP_CIPHER_CTX_ctrl(dctx,EVP_CTRL_GCM_SET_IVLEN,NONCE_LEN,NULL);
-
-EVP_DecryptInit_ex(dctx,NULL,NULL,key,nonce);
-
-
-
-EVP_DecryptUpdate(dctx,plain,&len,cipher,clen);
-
-plen=len;
-
-
-
-EVP_CIPHER_CTX_ctrl(dctx,EVP_CTRL_GCM_SET_TAG,TAG_LEN,tag);
-
-
-
-ret=EVP_DecryptFinal_ex(dctx,plain+plen,&len);
-
-
-
-EVP_CIPHER_CTX_free(dctx);
-
-
-
-if(ret<=0){
-
-    printf("TAG VERIFY FAILED\n");
+    printf("File received safely.\n");
 
     close(cfd);
-
-    return 1;
-
-}
-
-
-
-plen+=len;
-
-
-
-FILE *fp=fopen("received.out","wb");
-
-fwrite(plain,1,plen,fp);
-
-fclose(fp);
-
-
-
-printf("File received safely.\n");
-
-
-
-close(cfd);
-
-close(sfd);
-
-return 0;
-
+    close(sfd);
+    return 0;
 }
