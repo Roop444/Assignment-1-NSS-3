4ea3bc66676151c6ce6dd01a2aa81364c3ecd473
Roop444 <rupinderk@iiitd.ac.in>
2026-04-20 18:44:41 +0530

    Add common.h header file with function declarations

---
+++ common.h
@@ -0,0 +1,34 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+#include <openssl/sha.h>
+
+#include <gssapi/gssapi.h>
+
+#define PORT 5555
+#define NONCE_LEN 12
+#define TAG_LEN 16
+#define KEY_LEN 32
+
+void die(const char *msg);
+
+int send_all(int fd, void *buf, size_t len);
+int recv_all(int fd, void *buf, size_t len);
+
+int send_token(int fd, gss_buffer_t tok);
+int recv_token(int fd, gss_buffer_t tok);
+
+void derive_key(gss_ctx_id_t ctx, unsigned char key[32]);
+
+#endif
