72256d5b860b4b4f136619505e7345155e54c7d0
Roop444 <rupinderk@iiitd.ac.in>
2026-04-20 21:52:19 +0530

    Update common.c

--- common.c
+++ common.c
@@ -1,49 +1,119 @@
 #include "common.h"
 
-void die(const char *msg) {
+
+
+void die(const char *msg){
+
     perror(msg);
+
     exit(1);
+
 }
 
-int send_all(int fd, void *buf, size_t len) {
-    size_t done = 0;
-    while (done < len) {
-        ssize_t n = write(fd, (char*)buf + done, len - done);
-        if (n <= 0) return -1;
-        done += n;
+
+
+int send_all(int fd, void *buf, size_t len){
+
+    size_t done=0;
+
+    while(done<len){
+
+        int n=write(fd,(char*)buf+done,len-done);
+
+        if(n<=0) return -1;
+
+        done+=n;
+
     }
+
     return 0;
+
 }
 
-int recv_all(int fd, void *buf, size_t len) {
-    size_t done = 0;
-    while (done < len) {
-        ssize_t n = read(fd, (char*)buf + done, len - done);
-        if (n <= 0) return -1;
-        done += n;
+
+
+int recv_all(int fd, void *buf, size_t len){
+
+    size_t done=0;
+
+    while(done<len){
+
+        int n=read(fd,(char*)buf+done,len-done);
+
+        if(n<=0) return -1;
+
+        done+=n;
+
     }
+
     return 0;
+
 }
 
-int send_token(int fd, gss_buffer_t tok) {
-    uint32_t n = htonl(tok->length);
-    if (send_all(fd, &n, 4) < 0) return -1;
-    if (send_all(fd, tok->value, tok->length) < 0) return -1;
+
+
+int send_token(int fd, gss_buffer_t tok){
+
+    uint32_t n=htonl(tok->length);
+
+    send_all(fd,&n,4);
+
+    send_all(fd,tok->value,tok->length);
+
     return 0;
+
 }
 
-int recv_token(int fd, gss_buffer_t tok) {
+
+
+int recv_token(int fd, gss_buffer_t tok){
+
     uint32_t n;
-    if (recv_all(fd, &n, 4) < 0) return -1;
-    n = ntohl(n);
 
-    tok->length = n;
-    tok->value = malloc(n);
-    if (!tok->value) return -1;
+    recv_all(fd,&n,4);
 
-    if (recv_all(fd, tok->value, n) < 0) {
-        free(tok->value);
-        return -1;
-    }
+    n=ntohl(n);
+
+
+
+    tok->length=n;
+
+    tok->value=malloc(n);
+
+
+
+    recv_all(fd,tok->value,n);
+
     return 0;
+
+}
+
+
+
+void derive_key(gss_ctx_id_t ctx, unsigned char key[32])
+{
+    OM_uint32 maj, min;
+
+    gss_buffer_desc in;
+    gss_buffer_desc mic = GSS_C_EMPTY_BUFFER;
+
+    char label[] = "SFC_SESSION_KEY";
+
+    in.value = label;
+    in.length = strlen(label);
+
+    maj = gss_get_mic(
+        &min,
+        ctx,
+        GSS_C_QOP_DEFAULT,
+        &in,
+        &mic
+    );
+
+    if (maj != GSS_S_COMPLETE)
+        die("gss_get_mic failed");
+
+    SHA256((unsigned char *)mic.value, mic.length, key);
+
+    gss_release_buffer(&min, &mic);
 }
