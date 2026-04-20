6241d891d5674fe9a57b7df29c1763e9384ab485
Roop444 <rupinderk@iiitd.ac.in>
2026-04-20 22:08:01 +0530

    Update common.c

--- common.c
+++ common.c
@@ -1,95 +1,74 @@
+/* =========================
+   common.c
+   ========================= */
 #include "common.h"
 
-
-
-void die(const char *msg){
-
+void die(const char *msg)
+{
     perror(msg);
-
     exit(1);
-
 }
 
+void gss_die(const char *msg, OM_uint32 maj, OM_uint32 min)
+{
+    printf("%s failed (maj=%u min=%u)\n", msg, maj, min);
+    exit(1);
+}
 
+int send_all(int fd, void *buf, size_t len)
+{
+    size_t done = 0;
 
-int send_all(int fd, void *buf, size_t len){
-
-    size_t done=0;
-
-    while(done<len){
-
-        int n=write(fd,(char*)buf+done,len-done);
-
-        if(n<=0) return -1;
-
-        done+=n;
-
+    while (done < len) {
+        int n = write(fd, (char *)buf + done, len - done);
+        if (n <= 0)
+            return -1;
+        done += n;
     }
 
     return 0;
-
 }
 
+int recv_all(int fd, void *buf, size_t len)
+{
+    size_t done = 0;
 
-
-int recv_all(int fd, void *buf, size_t len){
-
-    size_t done=0;
-
-    while(done<len){
-
-        int n=read(fd,(char*)buf+done,len-done);
-
-        if(n<=0) return -1;
-
-        done+=n;
-
+    while (done < len) {
+        int n = read(fd, (char *)buf + done, len - done);
+        if (n <= 0)
+            return -1;
+        done += n;
     }
 
     return 0;
-
 }
 
+int send_token(int fd, gss_buffer_t tok)
+{
+    uint32_t n = htonl(tok->length);
 
-
-int send_token(int fd, gss_buffer_t tok){
-
-    uint32_t n=htonl(tok->length);
-
-    send_all(fd,&n,4);
-
-    send_all(fd,tok->value,tok->length);
+    send_all(fd, &n, 4);
+    send_all(fd, tok->value, tok->length);
 
     return 0;
-
 }
 
-
-
-int recv_token(int fd, gss_buffer_t tok){
-
+int recv_token(int fd, gss_buffer_t tok)
+{
     uint32_t n;
 
-    recv_all(fd,&n,4);
+    recv_all(fd, &n, 4);
+    n = ntohl(n);
 
-    n=ntohl(n);
+    tok->length = n;
+    tok->value = malloc(n);
 
-
-
-    tok->length=n;
-
-    tok->value=malloc(n);
-
-
-
-    recv_all(fd,tok->value,n);
+    recv_all(fd, tok->value, n);
 
     return 0;
-
 }
 
-
-
+/* Portable Phase 3 key derivation */
 void derive_key(gss_ctx_id_t ctx, unsigned char key[32])
 {
     OM_uint32 maj, min;
@@ -111,7 +90,7 @@ void derive_key(gss_ctx_id_t ctx, unsigned char key[32])
     );
 
     if (maj != GSS_S_COMPLETE)
-        die("gss_get_mic failed");
+        gss_die("gss_get_mic", maj, min);
 
     SHA256((unsigned char *)mic.value, mic.length, key);
 
